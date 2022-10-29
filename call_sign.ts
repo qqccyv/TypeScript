b6b2437b94ea360d05c7d015d9ed47ce2340e313
邓宇 <313187515@qq.com>
2022-10-29 23:22:58 +0800

    测试提交

--- call_sign.ts
+++ call_sign.ts
@@ -1,34 +1,32 @@
 // 用于描述一个可以被调用的函数
 
 type descreptionFunction = {
-  descreption: string
-  (someArg: number): boolean
-}
+  descreption: string;
+  (someArg: number): boolean;
+};
 // 使用场景比如需要描述一个回调函数
 function onCallback(callback: descreptionFunction) {
-  const number = 5
+  const number = 5;
 
   console.log(callback.descreption + callback(number));
-
 }
 
 function isEven(number: number): boolean {
   console.log(number % 2);
 
-  return number % 2 === 0
+  return number % 2 === 0;
 }
-isEven.descreption = 'this number is even:'
-onCallback(isEven)
-
+isEven.descreption = "this number is even:";
+onCallback(isEven);
 
 type callOrConstructor = {
   // 构造签名
-  new(date: string): Date
+  new (date: string): Date;
   // 调用签名
-  (n: number): number
-}
-
+  (n: number): number;
+};
+// 测试提交
 function callFunction(call: callOrConstructor) {
-  new call('2022-10-23')
-  call(Date.now())
+  new call("2022-10-23");
+  call(Date.now());
 }
